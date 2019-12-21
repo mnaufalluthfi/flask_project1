22b0d0e88a7203efe8e98ac8e22b8f54f8102204
mnaufalluthfi <naufalluthfi1012@gmail.com>
2019-12-21 12:43:47 +0700

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,abort,request
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return "Hello, Flask!"
 
+@app.route('/hello/')
+def hello(name=None):
+    if name is None:
+        name=request.args.get("name")
+        if name:
+            return "Hello, {}".format(name)
+        else:
+            abort(404)
+
 if __name__ == '__main__':
     app.run()
-    
